098ecffdf821165a04f5765653b807a31d9bdabf
D4rshan19 <dhimal.621185@swl.k12.oh.us>
2025-11-12 08:45:11 -0500

    Lab_09

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Random;
+import java.util.Scanner;
 
-public class Part_1 {
+public class Main {
+    public static void main(String[] args) {
+        //Randomizer and declaring int array values
+        Random rand = new Random();
+        int [] dataPoints = new int [100];
+
+        //Declaring scanner and variables for part 2
+        Scanner scan = new Scanner(System.in);
+
+
+        //Making the array
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rand.nextInt(100) + 1;
+        }
+        //Displaying array values
+        System.out.println("Array: ");
+        for (int x : dataPoints) {
+            System.out.print(x + " | ");
+        }
+    }
 }
+
+
+
+
+
