c9bf1df34caf96697b1a6178a4466a0114b4de5a
D4rshan19 <dhimal.621185@swl.k12.oh.us>
2025-11-14 08:55:26 -0500

    Lab_09

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,12 @@ public class Main {
         //Randomizer and declaring int array values
         Random rand = new Random();
         int [] dataPoints = new int [100];
+        int sum = 0;
+        int average = 0;
 
         //Declaring scanner and variables for part 2
         Scanner scan = new Scanner(System.in);
+        int counter = 0;
 
 
         //Making the array
@@ -19,7 +22,38 @@ public class Main {
         System.out.println("Array: ");
         for (int x : dataPoints) {
             System.out.print(x + " | ");
+
         }
+
+        int min = dataPoints[0];
+        int max = dataPoints[0];
+
+        for (int i = 0; i < dataPoints.length; i++) {
+            sum += dataPoints[i];
+            average = sum/100;
+            if (dataPoints[i] < min) {
+                min = dataPoints[i];
+            }
+            if (dataPoints[i] > max) {
+                max = dataPoints[i];
+            }
+        }
+            //Printing out specific results from the array
+            System.out.println("\nThe sum of all the elements is " + sum);
+            System.out.println("The average of all the elements is " + average);
+            System.out.println("The minimum of all the elements is " + min);
+            System.out.println("The maximum of all the elements is " + max);
+
+            //Asking user for input
+        int userInput = InputHelper.getRangedInt(scan, "Enter a value between 1 - 100" ,1, 100);
+
+        //Making for loop to count user input within array
+        for ( int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == userInput) {
+                counter++;
+            }
+        }
+        System.out.println("You entered " + userInput + " and it was displayed " + counter + " times throughout the array");
     }
 }
 
